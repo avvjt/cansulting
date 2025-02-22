3ad20466b2c68ebd983816f7eaab4e85e9d63ab2
Nishita Panchal <nishakp3005@gmail.com>
2025-02-22 18:31:11 +0530

    Add WordRotate component and integrate into Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Link from "next/link";
 import Image from "next/image";
 import { logo } from "@/public";
@@ -7,54 +8,64 @@ import { TextHover } from "@/animation";
 import { navbarItems } from "@/constants";
 import { useMotionValueEvent, useScroll, motion } from "framer-motion";
 import MobileNav from "./MobileNav";
-
+import WordRotate from "@/components/WordRotate";
 export default function Navbar() {
-	const [hidden, setHidden] = useState(false);
-	const { scrollY } = useScroll();
+    const [hidden, setHidden] = useState(false);
+    const { scrollY } = useScroll();
 
-	useMotionValueEvent(scrollY, "change", (latest) => {
-		const previous = scrollY.getPrevious();
-		if (previous && latest > previous) {
-			setHidden(true);
-		} else {
-			setHidden(false);
-		}
-	});
+    useMotionValueEvent(scrollY, "change", (latest) => {
+        const previous = scrollY.getPrevious();
+        if (previous && latest > previous) {
+            setHidden(true);
+        } else {
+            setHidden(false);
+        }
+    });
 
-	return (
-		<>
-			<motion.nav
-				variants={navVariants}
-				className="w-full h-[8vh] padding-x fixed top-0 left-0 z-50 backdrop-blur-[7px] flex items-center justify-between sm:hidden xm:hidden md:hidden"
-				animate={hidden ? "hidden" : "vissible"}>
-				<div className="w-[50%]">
-					<Link href={"/"}>
-						{/* <Image
+    return (
+        <>
+            <motion.nav
+                variants={navVariants}
+                className="w-full h-[8vh] padding-x fixed top-0 left-0 z-50 backdrop-blur-[7px] flex items-center justify-between sm:hidden xm:hidden md:hidden"
+                animate={hidden ? "hidden" : "vissible"}
+            >
+                <div className="w-[50%]">
+                    <Link href={"/"}>
+                        {/* <Image
 							src={logo}
 							alt="Influiditylogo"
 							width={70}
 							height={70}
 						/> */}
-						<p className="font-semibold text-2xl">Home</p>
-					</Link>
-				</div>
-				<div className="flex gap-x-[20px] w-[50%]">
-					{navbarItems.map((item) => (
-						<Link
-							key={item.id}
-							className={`w-fit paragraph font-medium font-NeueMontreal text-secondry capitalize flex flex-col hover ${
-								item.id === 5 && "ml-auto"
-							}`}
-							href={item.href}>
-							<TextHover
-								titile1={item.title}
-								titile2={item.title}
-							/>
-						</Link>
-					))}
-				</div>
-			</motion.nav>
-			<MobileNav />
-		</>
-	);
+                        <div className="flex items-center gap-x-1">
+                            <p className="font-semibold text-2xl">Home</p>
+                            <WordRotate
+                                words={["Creative", "Innovative", "Passionate"]}
+                                duration={1000}
+                                fontSize="1rem" 
+								className="translate-y-8"
+                            />
+                        </div>
+                    </Link>
+                </div>
+                <div className="flex gap-x-[20px] w-[50%]">
+                    {navbarItems.map((item) => (
+                        <Link
+                            key={item.id}
+                            className={`w-fit paragraph font-medium font-NeueMontreal text-secondry capitalize flex flex-col hover ${
+                                item.id === 5 && "ml-auto"
+                            }`}
+                            href={item.href}
+                        >
+                            <TextHover
+                                titile1={item.title}
+                                titile2={item.title}
+                            />
+                        </Link>
+                    ))}
+                </div>
+            </motion.nav>
+            <MobileNav />
+        </>
+    );
 }
